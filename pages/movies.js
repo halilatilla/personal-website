dd97c9a2d74691cff395f0983bb1eae122b9dce6
halilatilla <atillahalil0@gmail.com>
2020-04-19 01:30:15 +0300

    region added

--- pages/movies.js
+++ pages/movies.js
@@ -14,10 +14,13 @@ const MovieList = ({ movies }) => {
               <div className="movie-container" key={movie._id}>
                 <div>
                   <p className="movie-name"> {movie.name}</p>
-                  <p className="movie-date">{movie.date.split("-")[0]}</p>
+                  <p className="movie-date">
+                    {movie.date.split("-")[0]} -{" "}
+                    <span className="region"> {movie.region} </span>
+                  </p>
 
                   <a
-                    target="_black"
+                    target="_blank"
                     href={movie.imdbUrl}
                     className="movie-image-wrapper"
                   >
@@ -83,10 +86,14 @@ const MovieList = ({ movies }) => {
           font-size: 1.2rem;
           font-weight: bold;
           letter-spacing: 1px;
+          padding: 9px;
         }
         .movie-date {
           font-size: 1.2rem;
           letter-spacing: 1px;
+        }
+        .region {
+          text-transform: capitalize;
         }
         .movie-image-wrapper {
           overflow: hidden;
