5d2c625f0c82a62396d2b8ba5d109d16fad02445
halilatilla <atillahalil0@gmail.com>
2020-09-29 15:50:04 +0300

    site.config added, document.js updated

---
+++ site.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "Halil Atilla",
+  description: "Halil Atilla personal website",
+  siteUrl: "https://halilatilla.com/",
+  googleAnalytic: "X-XXXXXXXXXX",
+  author: {
+    email: "atillahalilo@gmail.com",
+    name: "Halil Atilla",
+  },
+  lang: "en",
+};
