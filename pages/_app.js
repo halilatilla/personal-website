a195c2a4ada55519b08912b29b5c24f4b2bef544
halilatilla <atillahalil0@gmail.com>
2020-09-30 17:05:21 +0300

    nprogress added

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Head from "next/head";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+import "nprogress/nprogress.css";
 import "../styles/global.scss";
+
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
 
 function MyApp({ Component, pageProps }) {
   return (
