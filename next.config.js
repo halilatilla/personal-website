91e11df4f14aa3eeeb61f0e7aca7296375f85967
halilatilla <atillahalil0@gmail.com>
2020-09-30 17:58:44 +0300

    pwa added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 // next.config.js
 require("dotenv").config();
-module.exports = {
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const nextConfig = {
   env: {
     API_KEY: process.env.API_KEY,
   },
+  pwa: {
+    dest: "public",
+    disable: isDev,
+  },
 };
+
+module.exports = withPlugins([], withPWA(nextConfig));
